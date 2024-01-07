533c703f4ae92762a5f2ef2a9114022bbab7ef22
Denis Bozhko <d.bozhko@chaika.com>
2024-01-07 13:33:34 +0300

    jobs deploy

--- distillery-test/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	 $(".menu-btn").on('click',function(){
-        $(".mobile-menu").slideToggle('fast');
-    });
-    $(".mobile-menu-close").on('click',function(){
-        $(".mobile-menu").slideUp('fast');
-    });
-});
--- readme.md
+++
@@ -1,9 +0,0 @@
-Hi.
-
-1) clone project to local folder
-
-2) npm install
-
-3) gulp
-
-Enjoy!
